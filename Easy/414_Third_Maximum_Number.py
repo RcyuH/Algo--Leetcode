f3ef326830f6ec1a22797f657bc5e6997b06215a
RcyuH <phunghuy.ms@gmail.com>
2025-07-03 15:51:45 +0700

    update 414

--- Easy/414_Third_Maximum_Number.py
+++ Easy/414_Third_Maximum_Number.py
@@ -6,3 +6,56 @@ Created on Thu Jul  3 13:52:40 2025
 @author: rcyuh
 """
 
+"""
+input: array nums
+
+output:
+        - third distinct maximum number
+        - not exist => return maximum number
+"""
+from typing import List
+
+def sol_0(nums):
+    pass
+    # Sort
+    # Traverse
+    # Return third distinct maximum number
+    # O(nlog(n))
+
+def sol_1(nums: List[int]) -> int:
+    try:
+        results = [nums[0], nums[1], nums[2]] 
+        for i in range(3):
+            if nums[i] > results[0]:
+                results[i] = results[0]
+                results[0] = nums[i]
+        if results[1] < results[2]:
+            tmp = results[1]
+            results[1] = results[2]
+            results[2] = tmp
+                
+    except:
+        try:
+            if nums[0] > nums[1]:
+                return nums[0]
+            else: return nums[1]
+        except:
+            return nums[0] 
+    
+    for i in range(3, len(nums)):
+        if nums[i] > results[0]:
+            results[2] = results[1]
+            results[1] = results[0]
+            results[0] = nums[i]
+            continue
+        
+        if results[1] == results[2] and nums[i] < results[2]: results[2] = nums[i]
+        else:
+            if nums[i] > results[2] and nums[i] < results[1]: results[2] = nums[i]
+    
+    if results[1] == results[2]:
+        return results[0]
+    else: return results[2]
+    
+    # O(n)
+        
